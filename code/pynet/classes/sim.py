4372764b648b5bcb2f93a7f102ebe4a0367edc52
Peng Zhou <peng.zhou137@gmail.com>
2016-11-01 00:13:14 +0100

    extend the methods of InterER; build Simulation class

---
+++ code/pynet/classes/sim.py
@@ -0,0 +1,17 @@
+from __future__ import division, print_function
+
+from .inter_er import InterER
+
+class Simulation(object):
+    """
+    Simulation class for cascading failure of interdependent network.
+    """
+
+    def __init__()
+
+    def step(self):
+        """
+        Performs the iteration for one cycle of removal.
+        """
+
+
