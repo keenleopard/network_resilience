a3db3bfa37fc51418a95b7534c3f80fb73522af3
Peng Zhou <peng.zhou137@gmail.com>
2016-11-06 00:18:43 +0100

    add scan of p value for fixed n

--- code/pynet/main.py
+++ code/pynet/main.py
@@ -2,26 +2,24 @@ from __future__ import division, print_function
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 from classes import InterER
 
 def main():
-    N = 200
+    N = 4000
     k = 4
 
-    G = InterER(N, k, k)
-    G.one2one()
-    nodes = G.fail(Q=1)
-    G.cascade()
-    print("failed:", nodes)
-    #print("nodes: ", G.nodes(), G.Ga.nodes(), G.Gb.nodes())
-    #print("edges: ", G.Ga.edges(), G.Gb.edges())
+    with open('frac_lmcc.dat', 'w') as f:
+        f.write('# p\t frac_lmcc\n')
+        for p in np.linspace(0.59, 0.63, num=100):
+            G = InterER(N,k,k)
+            G.one2one()
 
-    print(G.is_mutually_connected)
-    clusters = sorted(G.clusters_a, key = lambda cluster: -len(cluster))
-    print(len(clusters[0]))
-
-    #nx.draw_networkx(G)
-    #plt.show()
+            to_be_removed = int(N * (1-p))
+            G.fail(Q=to_be_removed)
+            G.cascade()
+            print(nx.number_of_nodes(G.Ga))
+            f.write(str(p) + '\t{0.frac_lmcc}\n'.format(G))
 
 
 if __name__ == "__main__":
