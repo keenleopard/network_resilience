456aad923317e28ba49e71e6f46ad70f82bcd273
Peng Zhou <peng.zhou137@gmail.com>
2016-11-05 18:09:08 +0100

    testing with main

--- code/pynet/main.py
+++ code/pynet/main.py
@@ -5,12 +5,24 @@ import matplotlib.pyplot as plt
 from classes import InterER
 
 def main():
-    N = 100
+    N = 200
     k = 4
 
     G = InterER(N, k, k)
-    nx.draw(G)
-    plt.show()
+    G.one2one()
+    nodes = G.fail(Q=1)
+    G.cascade()
+    print("failed:", nodes)
+    #print("nodes: ", G.nodes(), G.Ga.nodes(), G.Gb.nodes())
+    #print("edges: ", G.Ga.edges(), G.Gb.edges())
+
+    print(G.is_mutually_connected)
+    clusters = sorted(G.clusters_a, key = lambda cluster: -len(cluster))
+    print(len(clusters[0]))
+
+    #nx.draw_networkx(G)
+    #plt.show()
+
 
 if __name__ == "__main__":
     main()
