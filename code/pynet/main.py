45919ad41b96873a137e852c23c88fdc520bd3dc
Peng Zhou <peng.zhou137@gmail.com>
2016-11-06 12:20:24 +0100

    rename k to be k_avg and modify output layout

--- code/pynet/main.py
+++ code/pynet/main.py
@@ -7,19 +7,19 @@ from classes import InterER
 
 def main():
     N = 4000
-    k = 4
+    k_avg = 4
 
     with open('frac_lmcc.dat', 'w') as f:
-        f.write('# p\t frac_lmcc\n')
+        f.write('# p*<k>\t frac_lmcc\n')
         for p in np.linspace(0.59, 0.63, num=100):
-            G = InterER(N,k,k)
+            G = InterER(N,k_avg,k_avg)
             G.one2one()
 
             to_be_removed = int(N * (1-p))
             G.fail(Q=to_be_removed)
             G.cascade()
             print(nx.number_of_nodes(G.Ga))
-            f.write(str(p) + '\t{0.frac_lmcc}\n'.format(G))
+            f.write(str(p*k_avg) + '\t{0.frac_lmcc}\n'.format(G))
 
 
 if __name__ == "__main__":
