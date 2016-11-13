5e8fe7b78029779575494321622e5f6d2682d0b0
potato18z <lisic@student.ethz.ch>
2016-11-13 19:25:06 +0100

    change sf

--- code/pynet/classes/inter_sf.py
+++ code/pynet/classes/inter_sf.py
@@ -7,17 +7,17 @@ import random
 
 class InterSF(Inter_Network):
 
-    def __init__(self)
+    def __init__(self, lam = 3)
         """
         n: number of nodes of each sub network
         ka, kb: average degree of network a, b
         all parameters already defined in the base class inter_Network
         """
 
-        self.Ga = nx.scale_free_graph(self.n, pa)
-        self.Gb = nx.fast_gnp_random_graph(self.n, pb)
-        #self.Ga = nx.path_graph(n)
-        #self.Gb = nx.path_graph(n)
+        #self.Ga = nx.scale_free_graph(self.n, pa)
+	self.Ga = nx.random_powerlaw_tree(self.n, lam)
+	# random_powerlaw_tree(n, gamma=3, seed=None, tries=100) with gamma (float) – Exponent of the power law ; tries (int) – Number of attempts to adjust the sequence to make it a tree.
+        self.Gb = nx.fast_gnp_random_graph(self.n, lam)
         
         #nx.Graph.__init__(self, nx.disjoint_union(self.Ga, self.Gb)) // not really have the connected big graph
 
