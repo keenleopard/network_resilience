ba31f85a857e032ced1a045cf8655a39783bfeb2
Peng Zhou <peng.zhou137@gmail.com>
2016-12-17 18:58:18 +0100

    autonomous

--- code/pynet/test.py
+++ code/pynet/test.py
@@ -8,13 +8,20 @@ import time
 from classes import Inter_Network, InterRR, InterER
 
 def main():
-    N = 10
+    N = 100
     k_avg = 4
 
     testGraph = InterER(N, k_avg, k_avg)
+    testGraph.one2one()
 
-    nx.draw(testGraph)
-    plt.show()
+    autolist = testGraph.setAutonomousNode(autoFrac=0.1, method="random")
+    print(autolist)
+    print(testGraph.nodes())
+    print(testGraph.Ga.nodes())
+    print(testGraph.Gb.nodes())
+
+    removeFrac = 0.1
+    testGraph.attack_random(subnet='a', Q=int(N * removeFrac))
 
 if __name__ == "__main__":
     main()
