dbafb05ec71996c60cb22e9af972e69d69293eab
Peng Zhou <peng.zhou137@gmail.com>
2016-12-18 12:17:59 +0100

    rename testGraph as G

--- code/pynet/autonomous.py
+++ code/pynet/autonomous.py
@@ -11,17 +11,15 @@ def main():
     N = 100
     k_avg = 4
 
-    testGraph = InterER(N, k_avg, k_avg)
-    testGraph.one2one()
+    G = InterER(N, k_avg, k_avg)
+    G.one2one()
 
-    autolist = testGraph.setAutonomousNode(autoFrac=0.1, method="random")
+    autolist = G.setAutonomousNode(autoFrac=0.1, method="random")
     print(autolist)
-    print(testGraph.nodes())
-    print(testGraph.Ga.nodes())
-    print(testGraph.Gb.nodes())
 
     removeFrac = 0.1
-    testGraph.attack_random(subnet='a', Q=int(N * removeFrac))
+    G.attack_random(subnet='a', Q=int(N * removeFrac))
+    G.cascade(auto=True)
 
 if __name__ == "__main__":
     main()
